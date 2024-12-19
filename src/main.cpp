fe3cc5fc06736d7d91dafd125080b7158570344e
Massimo Sandretti <massimo.sandretti@gmail.com>
2024-12-19 13:45:54 +0000

    weak scalability setup

--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,24 @@
 
 
 
-const int maxSteps = 10000; // Maximum number of time steps
+const int maxSteps = 100; // Maximum number of time steps
 const double Re = 10000;
 const double u_lid = 0.5;
 
 const int MAX_THREADS = 12;
+const int WORK_PER_THREAD = 20000;
+
 const int ITERATIONS_PER_FRAME = 10;
 const int ITERATIONS_PER_PROGRESS_UPDATE = 100;
 
-const int NX = 100; // Dimension in the x-direction
-const int NY = 100; // Dimension in the y-direction
 
 int main() {
-    // Create the output file for velocity
-    std::ofstream file("vel_data.txt");
-    if (!file.is_open()) {
-        std::cerr << "could not opene/create 'vel_data.txt'.\n";
-        return 1;
-    }
-    file << NX << "\n" << NY << "\n";
 
     for(int threadsCount = 1; threadsCount <= MAX_THREADS; threadsCount++) {
+        int NX = sqrt(WORK_PER_THREAD * threadsCount);
+        int NY = NX;
+        while (NX*NY < WORK_PER_THREAD*threadsCount) NY++;
+        
         LBM lbm(NX, NY, u_lid, Re);
         omp_set_num_threads(threadsCount);
 
@@ -39,10 +36,8 @@ int main() {
 
         auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
 
-        std::cout << "Threads: " << threadsCount << " - " << "Time: " << elapsedTime << "ms" << std::endl;
+        std::cout << "Threads: " << threadsCount << " - " << "Cavity size: " << NY << "x" << NX << " - " << "Time: " << elapsedTime << "ms" << std::endl;
     }
-
-    file.close();
 
     std::cout << "\n";
 
